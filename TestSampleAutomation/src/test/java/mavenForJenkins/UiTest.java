c563dea0a2faeef23033ea5e682e10da06280f5b
automationQA <automationraviqa@gmail.com>
2021-06-02 13:24:00 -0400

    Updated files

--- TestSampleAutomation/src/test/java/mavenForJenkins/UiTest.java
+++ TestSampleAutomation/src/test/java/mavenForJenkins/UiTest.java
@@ -3,14 +3,26 @@ package mavenForJenkins;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class UiTest {
+    @Parameters("Browser")
     @Test
-    public void startBrowser() {
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+    public void startBrowser(String browserName) {
+        System.out.println("Parameter value is " + browserName);
+        WebDriver driver = null;
+        if (browserName.contains("chrome") || browserName.contains("CHROME")) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        }
+        else if (browserName.contains("Edge")) {
+            WebDriverManager.edgedriver().setup();
+            driver = new EdgeDriver();
+        }
+
         driver.manage().window().maximize();
         driver.get("https://opensource-demo.orangehrmlive.com/");
         Assert.assertTrue(driver.getTitle().contains("OrangeHRM"),"Title does not match");
